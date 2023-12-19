2da7e1c1d9bce9a28c1f3b7b69ae7711a525f56b
Hemat Waziri <hematullahw@gmail.com>
2023-12-19 10:36:29 +0430

    Level 1 & 2 Security

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/secretsDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const secKey = "Thisissecretkey.";
+
+userSchema.plugin(encrypt, {
+    secret: secKey,
+    encryptedFields: ["password"]
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save()
+        .then((result) => {
+            console.log(result);
+            res.render("secrets")
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.post("/login", (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+
+    User.findOne({ email: username })
+        .then((result) => {
+            if (result) {
+                if (result.password == password) {
+                    console.log(result);
+                    res.render("secrets")
+                } else {
+                    console.log("Icorrect Password!");
+                }
+            } else {
+                console.log("We couldn't found any user!");
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+
+app.listen(3000, () => {
+    console.log("Server Started in port 3000 🚀");
+})
